06dd411f27a683d6a600c9b5e318445953ed8fec
Maxim <astat259505@gmail.com>
2022-03-23 23:33:55 +0300

    feat: half simple-chain task done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,22 +4,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
+  result: '',
+  counter: 0,
   getLength() {
-   
+  return this.counter
   },
-  addLink(/value) {
-   
-  },
-  removeLink(/position) {
+  addLink(value) {
+   this.result = `${this.result}( ${value} )~~`
+   this.counter++
+   return this
     
   },
+  removeLink(position) {
+  let arr = this.result.split('~~')
+  this.result = arr.filter((item, index) => index + 1 != position).join('~~')
+  return this
+  },
   reverseChain() {
-   
+  console.log(this.result)
+  let arr = this.result.split('~~').reverse().filter((item, index) => index != 0)
+                       .map(item => `${item}~~`)
+  this.result = arr.join('')
+  return this
   },
   finishChain() {
- 
-  }
+  let str = this.result.slice(0, this.result.length - 2)
+  
+  this.result = ''
+  this.counter = 0
+  return str
+  },
+  
 };
 
 module.exports = {
