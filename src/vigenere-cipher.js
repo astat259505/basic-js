97f36e080cbbcf4ca86b588a2d376adee8250c65
Maxim <astat259505@gmail.com>
2022-03-27 01:14:50 +0300

    feat: cipher half task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  encrypt(message, key) {
+   let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+   message = message.toUpperCase().split('')
+   key = key.toUpperCase().split('')
+    let result = []
+    
+    let count = 0
+    for (let k = 0; k < message.length; k++) {
+      if (message[k].charCodeAt() >= 65 && message[k].charCodeAt() <= 90)
+        count++
+    }
+    console.log(count)
+    let arr2 = []
+  
+      for (let j = 0; j < count;j++) {
+        if (j <= 7) {
+      
+          arr2.push(arr.slice(arr.indexOf(key[j]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j])))))
+       
+        } if (j >= 8) {
+        arr2.push(arr.slice(arr.indexOf(key[j - 8]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - 8])))))
+         
+        }
+      }
+   let counter = 0
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        
+        result[i] = arr2[counter][arr.indexOf(message[i])]
+        counter ++
+      } else {
+        result[i] = message[i]
+      }
+    }
+ return result.join('')
   }
+    
+                               
+  
+  
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
   }
 }
 
