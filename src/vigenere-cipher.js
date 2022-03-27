07aa64826d785606ec727e29882622a1775e5223
Maxim <astat259505@gmail.com>
2022-03-27 19:07:39 +0300

    feat: cipher task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,7 +20,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
  class VigenereCipheringMachine {
+  constructor(condition) {
+    this.condition = condition
+  }
   encrypt(message, key) {
+    if (!message || !key) {
+      throw Error('Incorrect arguments!')
+    }
    let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
    message = message.toUpperCase().split('')
    key = key.toUpperCase().split('')
@@ -34,16 +40,16 @@ const { NotImplementedError } = require('../extensions/index.js');
     console.log(count)
     let arr2 = []
   
-      for (let j = 0; j < count;j++) {
-        if (j <= 7) {
-      
+       for (let j = 0; j < count;j++) {
+        if (j < key.length) {
           arr2.push(arr.slice(arr.indexOf(key[j]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j])))))
-       
-        } if (j >= 8) {
-        arr2.push(arr.slice(arr.indexOf(key[j - 8]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - 8])))))
-         
+        } if (j >= key.length && j % key.length == 0) {
+        arr2.push(arr.slice(arr.indexOf(key[j - ((j / key.length) * key.length)]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - ((j / key.length) * key.length)])))))  
+        } if  (j >= key.length && j % key.length != 0) {
+         arr2.push(arr.slice(arr.indexOf(key[j - (j - j % key.length)]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - (j - j % key.length)])))))   
         }
       }
+    
    let counter = 0
     for (let i = 0; i < message.length; i++) {
       if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
@@ -54,16 +60,67 @@ const { NotImplementedError } = require('../extensions/index.js');
         result[i] = message[i]
       }
     }
- return result.join('')
+  if (this.condition == false) {
+      return result.reverse().join('')
+    } else {
+  return result.join('')
   }
-    
+  }
                                
   
   
-  decrypt() {
-  
+  decrypt(message, key) {
+      if (!message || !key) {
+      throw Error('Incorrect arguments!')
+    }
+  let arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+   message = message.toUpperCase().split('')
+   key = key.toUpperCase().split('')
+
+   let result = []
+    
+    let count = 0
+    for (let k = 0; k < message.length; k++) {
+      if (message[k].charCodeAt() >= 65 && message[k].charCodeAt() <= 90)
+        count++
+    }
+      let arr2 = []
+
+      for (let j = 0; j < count;j++) {
+        if (j < key.length) {
+          arr2.push(arr.slice(arr.indexOf(key[j]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j])))))
+        } if (j >= key.length && j % key.length == 0) {
+        arr2.push(arr.slice(arr.indexOf(key[j - ((j / key.length) * key.length)]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - ((j / key.length) * key.length)])))))  
+        } if  (j >= key.length && j % key.length != 0) {
+         arr2.push(arr.slice(arr.indexOf(key[j - (j - j % key.length)]), arr.length).concat(arr.slice(0, (arr.indexOf(key[j - (j - j % key.length)])))))   
+        }
+      }
+    
+ 
+     let counter = 0
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        
+        result[i] = arr[arr2[counter].indexOf(message[i])]
+        counter++
+       
+      } else {
+        result[i] = message[i]
+      }
+    }
+    if (this.condition == false) {
+      return result.reverse().join('')
+    } else {
+  return result.join('')
   }
+  }
+
+
+
+  
 }
+
+
 
 module.exports = {
   VigenereCipheringMachine
